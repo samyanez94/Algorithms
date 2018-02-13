847dc6e5c3f403e44bdc87798b9cd0cfebab30a4
Samuel Yanez <samuelyanez94@gmail.com>
2018-02-13 11:35:15 -0800

    Improved tests in search algorithms

--- src/test/java/Search/BinarySearchTest.java
+++ src/test/java/Search/BinarySearchTest.java
@@ -2,8 +2,6 @@ package Search;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -15,29 +13,29 @@ class BinarySearchTest {
     // Test #1: Asserts the search method returns true when the target is in the list
     @Test
     void findsTarget() {
-        Integer[] list = { 1, 2, 3, 4, 5 };
+        Integer[] list = {1, 2, 3, 4, 5};
         assertTrue(BinarySearch.search(5, list));
     }
 
     // Test #2: Asserts the search method returns false when the target is not in the list
     @Test
     void doesNotFindsTarget() {
-        Integer[] list = { 1, 2, 3, 4, 5 };
+        Integer[] list = {1, 2, 3, 4, 5};
         assertFalse(BinarySearch.search(6, list));
     }
 
-    // Test #3: Asserts the search method returns false when the list is not sorted
+    // Test #3: Asserts the search method returns true when there are duplicates in the list
     @Test
-    void assertWhenListIsNotSorted() {
-        Integer[] list = { 4, 3, 2, 5, 8 , 1};
-        assertFalse(BinarySearch.search(4, list));
+    void findsTargetWithDuplicates() {
+        Integer[] list = {1, 1, 3, 3, 10, 10};
+        assertTrue(BinarySearch.search(3, list));
     }
 
     // Test #4: Asserts the search method returns true when other objects other than Integers are
     // used.
     @Test
     void findsTargetWithStrings() {
-        String[] list = { "Chad", "Luis", "Luis", "Nicolas", "Samuel"};
-        assertTrue(BinarySearch.search("Chad",list));
+        String[] list = {"Chad", "Luis", "Luis", "Nicolas", "Samuel"};
+        assertTrue(BinarySearch.search("Chad", list));
     }
 }
--- src/test/java/Search/LinearSearchTest.java
+++ src/test/java/Search/LinearSearchTest.java
@@ -1,6 +1,7 @@
 package Search;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -12,21 +13,21 @@ class LinearSearchTest {
     // Test #1: Asserts the search method returns true when the target is in the list
     @Test
     void findsTarget() {
-        Integer[] list = { 1, 2, 3, 4, 5 };
+        Integer[] list = {1, 2, 3, 4, 5};
         assertTrue(LinearSearch.search(5, list));
     }
 
     // Test #2: Asserts the search method returns false when the target is not in the list
     @Test
     void doesNotFindsTarget() {
-        Integer[] list = { 1, 2, 3, 4, 5 };
+        Integer[] list = {1, 2, 3, 4, 5};
         assertFalse(LinearSearch.search(6, list));
     }
 
     // Test #3: Asserts the search method returns true when there are duplicates in the list
     @Test
     void findsTargetWithDuplicates() {
-        Integer[] list = { 1, 1, 3, 3, 10, 10 };
+        Integer[] list = {1, 1, 3, 3, 10, 10};
         assertTrue(LinearSearch.search(3, list));
     }
 
@@ -34,7 +35,7 @@ class LinearSearchTest {
     // used.
     @Test
     void findsTargetWithStrings() {
-        String[] list = { "Samuel", "Luis", "Luis", "Nicolas", "Chad" };
+        String[] list = {"Chad", "Luis", "Luis", "Nicolas", "Samuel"};
         assertTrue(LinearSearch.search("Chad", list));
     }
 }
